b9361d0cddd9bbf984ae256f840f6841bce07753
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-12-16 18:31:03 +0100

    Add project files.

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibPlants
+{
+    public class Consumer : ConsumerBase
+    {
+        private string URL;
+
+        public Consumer(string url)
+        {
+            URL = url;
+        }
+
+        public async Task<TOut> GetAsync<TOut>()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.GetAsync(URL));
+            }
+        }
+
+        public async Task<TOut> GetAsync<TOut>(string uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.GetAsync(URL + uri));
+            }
+        }
+
+        public async Task PostAsync<TIn>(TIn item)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.PostAsync(URL, content));
+            }
+        }
+
+        public async Task<TOut> PostAsync<TIn, TOut>(TIn item)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.PostAsync(URL, content));
+            }
+        }
+
+        public async Task PostAsync<TIn>(TIn item, string uri)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.PostAsync(URL + uri, content));
+            }
+        }
+
+        public async Task<TOut> PostAsync<TIn, TOut>(TIn item, string uri)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.PostAsync(URL + uri, content));
+            }
+        }
+
+        public async Task PutAsync<TIn>(TIn item)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.PutAsync(URL, content));
+            }
+        }
+
+        public async Task<TOut> PutAsync<TIn, TOut>(TIn item)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.PutAsync(URL, content));
+            }
+        }
+
+        public async Task PutAsync<TIn>(TIn item, string uri)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.PutAsync(URL + uri, content));
+            }
+        }
+
+        public async Task<TOut> PutAsync<TIn, TOut>(TIn item, string uri)
+        {
+            StringContent content = EncodeContent(item);
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.PutAsync(URL + uri, content));
+            }
+        }
+
+        public async Task DeleteAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.DeleteAsync(URL));
+            }
+        }
+
+        public async Task<TOut> DeleteAsync<TOut>()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.DeleteAsync(URL));
+            }
+        }
+
+        public async Task DeleteAsync(string uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                await HandleHTTPResponseAsync(() => client.DeleteAsync(URL + uri));
+            }
+        }
+
+        public async Task<TOut> DeleteAsync<TOut>(string uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await HandleHTTPResponseAsync<TOut>(() => client.DeleteAsync(URL + uri));
+            }
+        }
+    }
+}
---
+++ Consumer/ConsumerBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ModelLibPlants
+{
+    public abstract class ConsumerBase
+    {
+        protected virtual async Task<TOut> HandleHTTPResponseAsync<TOut>(Func<Task<HttpResponseMessage>> requestFunc)
+        {
+            HttpResponseMessage response = await requestFunc();
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                TOut content = JsonConvert.DeserializeObject<TOut>(jsonString);
+                return content;
+            }
+            throw new HttpRequestException(response.StatusCode.ToString());
+        }
+
+        protected virtual async Task HandleHTTPResponseAsync(Func<Task<HttpResponseMessage>> requestFunc)
+        {
+            HttpResponseMessage response = await requestFunc();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException(response.StatusCode.ToString());
+            }
+        }
+        
+        protected virtual StringContent EncodeContent<TIn>(TIn item)
+        {
+            string jsonStr = JsonConvert.SerializeObject(item);
+            StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+            return content;
+        }
+    }
+}
